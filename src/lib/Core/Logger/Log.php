f3698fd70bc8c20879318c283099fa3cc5cf60d6
Konstantin Kondrashov <desanto@inbox.ru>
2026-04-04 09:57:57 +0200

    Логгер. Автоматическое создание папки и файла для логов.

---
+++ src/lib/Core/Logger/Log.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Softline\Core\Logger;
+
+enum Log: string
+{
+	case GENERAL = 'general';
+}
---
+++ src/lib/Core/Logger/Logger.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Softline\Core\Logger;
+
+use DateTime;
+
+class Logger
+{
+	public function add(string $message, Log $log): void
+	{
+		$message = $this->prepareMessage($message);
+
+		$this->writeToFile($message, $log);
+	}
+
+	private function prepareMessage(string $message): string
+	{
+		$now = DateTime::createFromFormat('U.u', microtime(true));
+		$prefix = $now->format('d-m-Y H:i:s.u');
+
+		return $prefix . ': ' . $message . PHP_EOL;
+	}
+
+	private function writeToFile(string $message, Log $log): void
+	{
+		$file = SRC_DIR . '/../logs/' . $log->value . '.log';
+
+		$dir = dirname($file);
+		if (!is_dir($dir))
+		{
+			mkdir($dir, 0777, true);
+		}
+
+		file_put_contents($file, $message, FILE_APPEND | LOCK_EX);
+	}
+}
